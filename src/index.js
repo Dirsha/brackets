65ecbc570d07f0aed4ada67def118dfb8e3c4e6e
Igor Dirsha <idirsha@gmail.com>
2020-10-24 16:42:15 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(expr, params) {
+    const holder = [];
+    let openBrackets = [],
+        closedBrackets = [],
+        dubleBrackets = [],
+        wowarr = ["`", "!", "@", "#", "$", "%", "^", "&", "*"],
+        dubleBracketslen = 0;
+    for (var i = 0; i < params.length; i++) {
+        openBrackets.push(params[i][0]);
+        if (params[i][0] == params[i][1]) {
+            closedBrackets.push(wowarr[dubleBracketslen]);
+            dubleBrackets.push(params[i][1]);
+            dubleBracketslen++;
+        } else {
+            closedBrackets.push(params[i][1]);
+        }
+    }
+
+    let exprArr = expr.split("");
+    let flag = 0;
+
+    for (let i = 0; i < dubleBrackets.length; i++) {
+        for (let j = 0; j < exprArr.length; j++) {
+            if (exprArr[j] == dubleBrackets[i]) {
+                if (flag > 0) {
+                    exprArr[j] = wowarr[i];
+                    flag--;
+                } else {
+                    flag++;
+                }
+            }
+        }
+    }
+    expr = exprArr.join("");
+
+    for (let letter of expr) {
+        if (openBrackets.includes(letter)) {
+            holder.push(letter);
+        } else if (closedBrackets.includes(letter)) {
+            const openPair = openBrackets[closedBrackets.indexOf(letter)];
+            if (holder[holder.length - 1] === openPair) {
+                holder.splice(-1, 1);
+            } else {
+                holder.push(letter);
+            }
+        }
+    }
+    return holder.length === 0;
+};
